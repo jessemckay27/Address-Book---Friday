d0d3a335de6323654436a80cb5effec60fc94ec9
Jesse McKay <rosecity27@comcast.net>
2016-11-18 11:22:42 -0800

    add empty file structure

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,14 @@
+using Nancy;
+using Address.Objects;
+using System.Collections.Generic;
+
+namespace Address
+{
+  public Class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+
+    }
+  }
+}
